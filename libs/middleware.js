e63bf647c805423bb344b54188aa9493200a695d
Sylvio <sylvio.migliorucci@gmail.com>
2018-10-25 22:24:48 -0300

    problema de login

--- libs/middleware.js
+++ libs/middleware.js
@@ -1,13 +1,17 @@
 import bodyParser from "body-parser";
+import express from "express";
+import cors from "cors";
+
 
 module.exports = app =>{
     app.set("port", 3000);
     app.set("json spaces", 4);
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
     app.use((req, res, next) => {
         delete req.body.id;
         next();
     });
-        
+    app.use(express.static("public")); 
 }
